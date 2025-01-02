27efb1f3428a2a3cb394c698c4792702dc66e965
Andreas Näsman <andreas.z.nasman@gmail.com>
2025-01-02 10:17:09 +0200

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<svelte:head>
+  <meta
+    content="This page displays a link to SvelteKit's documentation."
+    name="description"
+  >
+  <title>Welcome to SvelteKit</title>
+</svelte:head>
+
+<h1>Welcome to SvelteKit</h1>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read
+  the documentation
+</p>
